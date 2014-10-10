0a0b4ab1fd45865eeadfd521097651a80e7350d9
dangeryoder <dangeryoder@gmail.com>
2014-10-10 14:36:45 -0500

    lab 1

---
+++ lab 1.py
@@ -0,0 +1,8 @@
+ 2print "hello what is your name?"
+print "type any two numbers"
+myFirstVariable = raw_input() 
+mySecondVariable = raw_input()
+myThirdVariable = raw_input()
+print myFirstVariable
+print int(mySecondVariable) + int(myThirdVariable)
+
